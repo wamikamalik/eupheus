ffdb38d1dddac6c6dd48bd126686f0dcc2a6cd9d
LAPTOP-K3LA6L88\wamik <wamikamalik@hotmail.com>
2021-01-17 07:36:20 +0400

    add init project files

---
+++ env.js
@@ -0,0 +1,14 @@
+export const dialogflowConfig = 
+{
+    "type": "service_account",
+    "project_id": "calcium-rigging-301904",
+    "private_key_id": "c83c34fce89c33cb335d6a99aea02970f4e98042",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCTdC4CgnJuAAzO\nXUrS3doqE/GDJAMTzE9C2nYH2GeRywjNgYcBNERmwdZ7mL7xs/q+QUboBOHrSGqr\nZPA5xFvzqSY9LwplNyYojXT1d8Pc6Xau7dhxU5iInRGBeY3Zxja0lgs6o85ljWsl\nTatRpFpgEJG4sCP2cs7lKBMtaSUYc1WEfjzT9vmVJfdX71+L9LRMGu/KD3FSHYs3\nQ2rLwJByz+Tgp9FY9dK6puGxE5Df43gqugHRIiAOp8EyLB10o5TQiWJDkCfIr8GN\n3inTRnM6DtRhlHvjdhApZ4f5O6iogr5QDiePsqAn4WOy/RsyUdsghW2XJ/5bps7/\nuLzxZ97dAgMBAAECggEADT1QD3c5YXMTs0iUU7ajxHkIWFpyN31Cwjsg5rduWGk+\n7LGfafyqOTSL8FdyQ/vn0A/GynjI9PoKY3VFGvEfwhMDLr8u7PbOSeamavV9jc2D\nxu67XmfyuPsRYh5RHdqhEtN3rD44Mmbhb5Bq74IVGa1v+05yP2PNLxPLSDdTcCYL\nOC9PSQQCsRSuHNhnkCCfARpsEr8K/3HuQqM+TONY9pe6HnORgGQ+PBLYxobQrqGA\nAkYTbGTBrOuKExYh/po86tWDnV+fbn7dg2tIYNmzxXMoyEopTwGEkU2YR7HGUWX5\nugd5WKhVB43wWOo4OCjMrL2yIflo92A5fLffHnZh6QKBgQDE+7J8JkKRd7IZhRx6\nHBv8e18PAx78TPbpfU98pNfgeuazNXkjHnifdORSFSoR8nGOvgnr3/XCyjaBKB/G\nxD6nlT4dVIcEO/QdRKAYWapIt/mwfozIJJxBDfSVmAe8CRgnDQHCM0wv+n+LPaS7\nFev2Pi378t0gju2WdHf+SPVAGwKBgQC/oahc3T4e+rpugaxnHEY/s6U4w0DzxaVH\nTG3gibcQQYaiHL5e17Ein1mx2Tg0VQNvEF3o1U4RopqJWARtCTQil2+aN4NesZy3\n1WFnLH+eAcW5mjJat/Vf9ci9Nn7+6dB3EW95l/siTvpguwzWIb+Ql3rNLEYTIyRY\nMR8Pyep8ZwKBgDl5fyB+QVoUZKDRl/eBECFhFeFZdccD5d9GstSZfGuBMKMFbSz0\nDRnpj1zr0/ScmsmdZWOvWxPU0IZq4KqsQ+O9xfvk1+hCXf3UMioor2i8VVxL8GPN\nWtIYck7bpxYUSTnLr8GsGgS9wE0uVUxJHyogBmbG/AIt8eC+ZuyI+RYVAoGBAJrx\nnO4pPVdTJ3a+hqWAaRSMrqTsyxHP10mE9Lq+3zPXp4Jgd1S8Vc9GxQFWP2mg63vb\n4fKmtc7wiL9QWZPIA02M4IHzLNdeCPdjIrSHeFBL4pGlqd1MLKyAUOUcsSZv60y6\n3kWUq0wcS7n2w/RHqVrCAFrw12AV5AcjAJiFjIE1AoGACWb9eEANfkuWPj5f5uuj\ntnM0n04xxARVAcCQ6f85kYYyxs8G0HWacIKgYvUp/GouSgAo8iPFVlPD7Um/Mad9\nAd62p5iI2S7nOkLg1v8EvxNGaoBMo6GP+EQ0nn3ZIebg4YaTCxt/jZxvYItdb/93\n9vlDfSr5siTM3jruzY7YUeE=\n-----END PRIVATE KEY-----\n",
+    "client_email": "eupheus@calcium-rigging-301904.iam.gserviceaccount.com",
+    "client_id": "117288255887607766315",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/eupheus%40calcium-rigging-301904.iam.gserviceaccount.com"
+  }
+  
