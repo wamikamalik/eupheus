c16b317dd8716c2fe6a60f964012031baa22a85c
LAPTOP-K3LA6L88\wamik <wamikamalik@hotmail.com>
2021-01-17 07:38:49 +0400

    add init App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import { Dialogflow_V2 } from 'react-native-dialogflow';
+
+import { dialogflowConfig } from './env';
+
+
+class App extends Component {
+  
+}
+
+export default App;
